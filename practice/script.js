e7ec4aae63b1750ef61aa86dd768f25c92d8e216
Rahil <syedrahil200@gmail.com>
2025-05-30 12:43:42 +0530

    Keyed collection pt1

--- practice/script.js
+++ practice/script.js
@@ -1,12 +1,14 @@
-for (let i = 0; i <= 100; i++) {
-    
-    if (i % 3 ===0 && i % 5 === 0) {
-        console.log("Fizz Buzz")
-    }else if (i % 3 === 0) {
-        console.log("Fizz")
-    }else if (i % 5 === 0) {
-        console.log("Buzz")
-    }else{
-        console.log(i)
-    }
+let Quotes = new Map()
+Quotes.set("Ghandhi", "An eye for an eye makes the whole world blind")
+Quotes.set("Einstein", "Nuclear power is one hell of a way to boil water")
+Quotes.set("Suntzu", "Cum quick")
+Quotes.set("Cooper", "When we were young we used to look up and wonder about our place in the stars now we look down and wonder about our place in the dirt")
+Quotes.set("Oppenheimer", "Now i have become death the destroyer of worlds")
+for (const [speaker,quote]of Quotes) {
+    let bod = document.getElementById('tbody')
+    newBod = `<tr>
+          <td>${speaker}</td>
+          <td>${quote}</td>
+        </tr>`
+    bod.innerHTML += newBod
 }
